5ad9ca6dd734d037e7d8553c93583f841c2c5bd5
Jay Colson <jay.colson@synchronoss.com>
2021-04-26 16:36:58 +0100

    added passive perception macro

---
+++ passivePerception.js
@@ -0,0 +1,28 @@
+//console.debug(game.actors.entries);
+let ppInfo = '';
+for (actor of game.actors.entries) {
+    if (actor.data.type == 'character') {
+        // console.debug(`actor:`, actor.data);
+        // console.debug(`actor: ${actor.data.name}`);
+        let playerName;
+        for (let playerKey of Object.getOwnPropertyNames(actor.data.permission)) {
+            let player = game.users.get(playerKey);
+            if (player && !player?.isGM) {
+                // console.debug(`player ${player.name}`);
+                playerName = player.name;
+            }
+        }
+        ppInfo += `${playerName ? playerName + ":" : ''}${actor.data.name}:PP:${actor.data.data.skills.per.passive}\n`
+    }
+}
+printMessage(`${ppInfo}`);
+function printMessage(message) {
+    let chatData = {
+        user: game.user._id,
+        content: message,
+        blind: true,
+        whisper: game.users.entities.filter(u => u.isGM).map(u => u._id)
+    };
+
+    ChatMessage.create(chatData, {});
+}
